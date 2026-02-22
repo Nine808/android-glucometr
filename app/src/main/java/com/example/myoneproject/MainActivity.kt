18985d3b6f0cf9f28b439c2975cb2ef29e71bd18
Nine <_EMAIL__GITHUB>
2026-02-22 15:39:37 +0700

    Разрешение для вкл Bluetooth

--- app/src/main/java/com/example/myoneproject/MainActivity.kt
+++ app/src/main/java/com/example/myoneproject/MainActivity.kt
@@ -1,16 +1,38 @@
 package com.example.myoneproject
 
+import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothManager
+import android.bluetooth.le.BluetoothLeScanner
+import android.bluetooth.le.ScanCallback
+import android.bluetooth.le.ScanFilter
+import android.bluetooth.le.ScanResult
+import android.bluetooth.le.ScanSettings
 import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.os.ParcelUuid
+import android.util.Log
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.util.UUID
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var bluetoothAdapter: BluetoothAdapter
+    private lateinit var bleScanner: BluetoothLeScanner
+    private val enableBluetoothLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK) {
+                startScan()
+            } else {
+                Toast.makeText(this, "Bluetooth не включён", Toast.LENGTH_SHORT).show()
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -20,9 +42,8 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(this, "BLE не поддерживается", Toast.LENGTH_SHORT).show()
             finish()
         }
-
         val bluetoothManager = getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
-        val bluetoothAdapter = bluetoothManager.adapter
+        bluetoothAdapter = bluetoothManager.adapter
 
         if (bluetoothAdapter == null || !bluetoothAdapter.isEnabled) {
             Toast.makeText(this, "Bluetooth выключен", Toast.LENGTH_SHORT).show()
@@ -32,6 +53,84 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        requestBlePermissions()
+    }
+
+    private fun enableBluetooth() {
+        val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+        enableBluetoothLauncher.launch(enableBtIntent)
+    }
+
+    private val scanCallback = object : ScanCallback() {
+        override fun onScanResult(callbackType: Int, result: ScanResult) {
+            val device = result.device
+            Log.d("BLE", "Найдено устройство: ${device.name} ${device.address}")
+        }
+    }
+    private fun startScan() {
+
+        if (!bluetoothAdapter.isEnabled) {
+            enableBluetooth()
+            return
+        }
+
+        if (checkSelfPermission(android.Manifest.permission.BLUETOOTH_SCAN)
+            != PackageManager.PERMISSION_GRANTED) {
+            return
+        }
+
+        bleScanner = bluetoothAdapter.bluetoothLeScanner
+
+        if (bleScanner == null) {
+            Toast.makeText(this, "Scanner недоступен", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        val filter = ScanFilter.Builder()
+            .setServiceUuid(
+                ParcelUuid(
+                    UUID.fromString("0000181F-0000-1000-8000-00805F9B34FB")
+                )
+            )
+            .build()
+
+        val settings = ScanSettings.Builder()
+            .setScanMode(ScanSettings.SCAN_MODE_LOW_LATENCY)
+            .build()
+
+        bleScanner.startScan(listOf(filter), settings, scanCallback)
+
+        Log.d("BLE", "Сканирование запущено")
+    }
+    private fun requestBlePermissions() {
+        requestPermissions(
+            arrayOf(
+                android.Manifest.permission.BLUETOOTH_SCAN,
+                android.Manifest.permission.BLUETOOTH_CONNECT
+            ),
+            100
+        )
+    }
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == 100) {
+            if (grantResults.isNotEmpty() &&
+                grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                startScan()
+            }
+        }
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == 200 && resultCode == RESULT_OK) {
+            startScan()
         }
     }
 }
